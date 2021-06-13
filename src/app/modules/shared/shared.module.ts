b6000d587afc810d9e564cfb5c6b2ef5e1915b64
Cesar Castillo <ccastillo@datafierce.com>
2021-06-13 11:25:20 -0500

    creando un modulo y component

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotesComponent } from './notes/notes.component';
+
+
+
+@NgModule({
+  declarations: [NotesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
