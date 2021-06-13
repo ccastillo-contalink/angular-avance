1786dab1691a5443779bef69060645c936b3c371
Cesar Castillo <ccastillo@datafierce.com>
2021-06-13 15:28:41 -0500

    mapeando los componentes en cada routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExternalComponentComponent } from './components/external-component/external-component.component';
 import { InternalComponentComponent } from './components/internal-component/internal-component.component';
 import { WebComponentComponent } from './components/web-component/web-component.component';
+import { RoutingParamsComponent } from './components/routing-params/routing-params.component';
+import { RoutingQueryParamsComponent } from './components/routing-query-params/routing-query-params.component';
 
 const routes: Routes = [{
   path: 'componente_externos',
@@ -13,6 +15,12 @@ const routes: Routes = [{
 }, {
   path: 'componentes_externo_web',
   component: WebComponentComponent
+}, {
+  path: 'routing_params',
+  component: RoutingParamsComponent
+}, {
+  path: 'routing_query_params',
+  component: RoutingQueryParamsComponent
 },
 {
   path: 'module_carga_lenta',
