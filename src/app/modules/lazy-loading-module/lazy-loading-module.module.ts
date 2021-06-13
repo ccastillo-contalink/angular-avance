db67d2fc2d2a6a3dc4481cf89835ceea83023366
Cesar Castillo <ccastillo@datafierce.com>
2021-06-13 14:54:36 -0500

    colocando la nota de lazy loading

--- src/app/modules/lazy-loading-module/lazy-loading-module.module.ts
+++ src/app/modules/lazy-loading-module/lazy-loading-module.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LazyLoadingModuleRoutingModule } from './lazy-loading-module-routing.module';
 import { LazyLoadingModuleComponent } from './lazy-loading-module.component';
 import { DataTablesModule } from "angular-datatables";
+import {SharedModule } from './../../modules/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { DataTablesModule } from "angular-datatables";
   imports: [
     CommonModule,
     LazyLoadingModuleRoutingModule,
-    DataTablesModule
+    DataTablesModule,
+    SharedModule
   ]
 })
 export class LazyLoadingModuleModule { }
