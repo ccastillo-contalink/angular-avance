df2edd0888a2eab59733ace1ad9d33b07ccd0258
Cesar Castillo <ccastillo@datafierce.com>
2021-06-13 22:39:31 -0500

    creando el objecto

--- src/app/components/routing-params/routing-params.component.ts
+++ src/app/components/routing-params/routing-params.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User} from '../../model/user';
 
 @Component({
   selector: 'app-routing-params',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./routing-params.component.css']
 })
 export class RoutingParamsComponent implements OnInit {
+
+  public user: User= {
+    id:'1',
+    address_id: '1',
+    name:'Sr Angular'
+  }
 
   constructor() { }
 
