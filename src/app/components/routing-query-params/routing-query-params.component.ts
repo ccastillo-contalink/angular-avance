4e11cb119dad0f24cb6ef946f3071059e1d2dd05
Cesar Castillo <ccastillo@datafierce.com>
2021-06-15 08:50:12 -0500

    query params con observable}

--- src/app/components/routing-query-params/routing-query-params.component.ts
+++ src/app/components/routing-query-params/routing-query-params.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-routing-query-params',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoutingQueryParamsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activated: ActivatedRoute) {
+
+   }
 
   ngOnInit() {
+    this.activated.queryParams.subscribe(value=> {
+      console.log(value);
+    })
   }
 
 }
