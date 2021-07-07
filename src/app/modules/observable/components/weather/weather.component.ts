d12d35084ebc50dd529c57250b82ae3fcbac661a
Cesar Castillo <ccastillo@datafierce.com>
2021-07-07 13:42:43 -0500

    usando un timer para el cambio de valores

--- src/app/modules/observable/components/weather/weather.component.ts
+++ src/app/modules/observable/components/weather/weather.component.ts
@@ -11,23 +11,40 @@ export class WeatherComponent implements OnInit {
   weather_syndney$: Observable<number>;
   weather_syndney: number;
 
-  constructor() { 
+  weather_melbourne$: Observable<number>;
+
+
+  weather_lithgow$: Observable<number>;
+  weather_lithgow: number;
+
+
+  constructor() {
     this.weather_syndney$ = this.getWeatherSydney();
+    this.weather_melbourne$ = this.getMelbourne();
+    this.weather_lithgow$ = this.getLithgow();
+
   }
 
   ngOnInit() {
 
-    this.weather_syndney$.subscribe(item=> {
-      this.weather_syndney= item;
+    this.weather_syndney$.subscribe(item => {
+      this.weather_syndney = item;
+
+    });
+
+
+    //Va seguir escuchando
+    this.weather_lithgow$.subscribe(item => {
+      this.weather_lithgow = item;
 
     })
   }
 
 
-  getWeatherSydney(): Observable<number>{
+  getWeatherSydney(): Observable<number> {
     let weather = this.getRandom();
 
-    return new Observable(observer=> {
+    return new Observable(observer => {
       observer.next(weather);
       observer.complete();
     })
@@ -35,9 +52,45 @@ export class WeatherComponent implements OnInit {
   }
 
 
+  getMelbourne(): Observable<number> {
+    let weather = this.getRandom();
 
-  getRandom(): number{
-    return  Math.floor(Math.random() * (40 - 5 + 1) + 5);
+    return new Observable(observer => {
+      observer.next(weather);
+      observer.complete();
+    })
+
+  }
+
+  getLithgow(): Observable<number> {
+
+    return new Observable(observer => {
+
+      observer.next(this.getRandom());
+
+      setTimeout(()=> {
+        observer.next(this.getRandom());
+
+      }, 4000);
+
+
+      setTimeout(()=> {
+        observer.next(this.getRandom());
+
+      }, 8000);
+
+
+      setTimeout(()=> {
+        observer.complete();
+
+      }, 12000);
+
+    })
+
+  }
+
+  getRandom(): number {
+    return Math.floor(Math.random() * (40 - 5 + 1) + 5);
   }
 
 }
