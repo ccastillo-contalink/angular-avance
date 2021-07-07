bc9187751798883da2b4571c53beb317310f5d56
Cesar Castillo <ccastillo@datafierce.com>
2021-07-07 13:24:56 -0500

    el observable mas simple

--- src/app/modules/observable/components/weather/weather.component.ts
+++ src/app/modules/observable/components/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  weather_syndney$: Observable<number>;
+  weather_syndney: number;
+
+  constructor() { 
+    this.weather_syndney$ = this.getWeatherSydney();
+  }
 
   ngOnInit() {
+
+    this.weather_syndney$.subscribe(item=> {
+      this.weather_syndney= item;
+
+    })
+  }
+
+
+  getWeatherSydney(): Observable<number>{
+    let weather = this.getRandom();
+
+    return new Observable(observer=> {
+      observer.next(weather);
+      observer.complete();
+    })
+
+  }
+
+
+
+  getRandom(): number{
+    return  Math.floor(Math.random() * (40 - 5 + 1) + 5);
   }
 
 }
